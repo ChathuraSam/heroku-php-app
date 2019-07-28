2c65fc06236df7e8f2ee39bbf29a8a048c352575
ChathuraSam <chathura.samarajeewa@gmail.com>
2019-07-29 01:31:14 +0530

    changed view 1

--- index.php
+++ index.php
@@ -33,15 +33,21 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<form class="login100-form validate-form">
-					<span class="login100-form-title p-b-26">
-						Welcome
+					
+				<span class="login100-form-title p-b-48">
+						<img src="images/google-logo.png" alt="" width="40%">
 					</span>
-					<span class="login100-form-title p-b-48">
-						<i class="zmdi zmdi-font"></i>
+				<span class="login100-form-title p-b-26">
+						Sign in
 					</span>
+					<span class="text-center">
+						<p>To continue to gmail</p>
+						
+					</span>
+					
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
-						<input class="input100" type="text" name="email">
+						<input class="input100" type="text" name="email" value="achindarangana@gmail.com">
 						<span class="focus-input100" data-placeholder="Email"></span>
 					</div>
 
@@ -53,14 +59,9 @@
 						<span class="focus-input100" data-placeholder="Password"></span>
 					</div>
 
-					<div class="container-login100-form-btn">
-						<div class="wrap-login100-form-btn">
-							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn">
+					<button class="btn btn-primary">
 								Login
 							</button>
-						</div>
-					</div>
 
 					<div class="text-center p-t-115">
 						<span class="txt1">
